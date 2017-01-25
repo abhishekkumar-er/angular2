78b4da8e2731b8dca88a98fe5d0b15c3bae4290e
Abhishek Kumar <abhishekkumar1985@hotmail.com>
2017-01-25 17:08:07 +0530

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import * as path from 'path';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <user></user>
+  `,
+})
+export class AppComponent  { 
+}
---
+++ app/components/user.component.ts
@@ -0,0 +1,73 @@
+import { NgModel } from '@angular/forms/src/directives';
+import { InputDecorator } from '@angular/core/src/metadata/directives';
+import { NgIf } from '@angular/common/src/directives/ng_if';
+import { NgFor } from '@angular/common/src/directives/ng_for';
+import * as path from 'path';
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'user',
+    template: `
+  <h1>{{name}}</h1>
+  <p> <strong>Email: </strong>{{email}} </p>
+  <p><strong> Address: </strong> {{address.street}}, {{address.city}}, {{address.state}}</p>
+  <button (click)="toggleHobbies()">{{showHobbies ? "Hide Hobbies" : "Show Hobbies"}}</button>
+  <div *ngIf='showHobbies'>
+  <h3>Hobbies:</h3>
+  <ul>
+  <li *ngFor="let hobby of hobbies; let i=index">{{hobby}} <button (click)="deleteHobby(i)">X</button></li>
+  </ul>
+  <form (submit)="addHobby(hobby.value)">
+  <label> Add Hobby: </label><br />
+  <input type="text" #hobby/><br />
+  </form>
+  </div>
+  <hr />
+  <h3> Edit User </h3>
+  <form>
+  <label> Name: </label>
+  <input type="text" name="name" [(ngModel)]="name" /><br />
+  <label> Email: </label> 
+  <input type="email" name="email" [(ngModel)]="email" /><br />
+  <label> Street: </label>
+  <input type="text" name="address.street" [(ngModel)]="address.street" /><br />
+  <label> City: </label>
+  <input type="text" name="address.city" [(ngModel)]="address.city" /><br />
+  <label> State: </label>
+  <input type="text" name="address.state" [(ngModel)]="address.state" /><br />
+  </form>
+  `,
+})
+export class UserComponent {
+    name: string ;
+    email: string ;
+    address: address;
+    hobbies: string[];
+    showHobbies: boolean;
+    constructor() {
+        this.name = 'Abhishek';
+        this.email = 'abhi@gmail.com';
+        this.address = {
+            street: 'pragati layout',
+            city: 'Bangalore',
+            state: 'KA',
+        }
+        this.hobbies =['Music', 'Movie', 'Sport'];
+        this.showHobbies = false;
+    }
+    toggleHobbies(){
+        return this.showHobbies=this.showHobbies?false:true;
+    }
+    addHobby(hobby) {
+        this.hobbies.push(hobby)
+    }
+    deleteHobby(i) {
+        this.hobbies.splice(i,1);
+    }
+    
+}
+interface address {
+        street: string;
+        city: string;
+        state:string;
+    }
